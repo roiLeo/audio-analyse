b416a49049683f538a0223aef722d73ba03651a3
roiLeo <medina.leo42@gmail.com>
2025-07-28 17:05:23 +0200

    ➖ unused dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   app: {
     head: {
-      title: 'Open Meta',
+      title: 'Audio Analyse',
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
     },
